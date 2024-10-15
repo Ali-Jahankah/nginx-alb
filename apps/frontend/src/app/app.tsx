2bfffaef2ba5dad1a01c56e64146d8a41ec94f09
Ali Jahankah <aral2@yahoo.com>
2024-10-15 14:27:41 +0100

    feat: setup nx project with Docker and Nginx as a load balancer

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,14 +1,26 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import { useEffect, useState } from "react";
 
-import NxWelcome from './nx-welcome';
+export const App = () => {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("http://localhost:4008/api");
+      const { message } = await res.json();
+      setMessage(message);
+    };
+    getData();
+  }, []);
 
-export function App() {
-  return (
-    <div>
-      <NxWelcome title="frontend" />
+  return message.length ? (
+    <div className="article">
+      <h1 className="message">{message}</h1>
+      <footer className="footer">
+        Created by <a href="https://github.com/Ali-Jahankah">Ali Jahankhah</a>
+      </footer>
     </div>
+  ) : (
+    <div>Server is not connected</div>
   );
-}
+};
 
 export default App;
